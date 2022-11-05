4a6ecec92a8fe5bfe20c477b54a3a1f1bb993033
Ad4mCZ <biciste.adam@ssakhk.cz>
2022-11-06 00:59:39 +0100

    ReadingFiles

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define MAX_LINES 100
+#define MAX_LENGTH 20
+
+int main()
+{
+    char data[MAX_LINES][MAX_LENGTH] = {{0}}; // 2d pole pro uchovavani radku
+    char otazky[MAX_LINES][MAX_LENGTH] = {{0}};
+
+    FILE *file = fopen("questions.txt", "r"); // otevreni souboru
+    if (file == NULL)                         // kontrola jestli existuje
+    {
+        printf("no such file.");
+        return 1;
+    }
+
+    int line = 0; // pocitani radku
+
+    while (!feof(file) && !ferror(file)) // pokud neni end of file nebo chyba
+    {
+        if (fgets(data[line], MAX_LENGTH, file) != NULL) // kontrola jestli nejsme na konci
+        {
+            line++;
+        }
+    }
+    fclose(file); // zavreni souboru
+
+    for (int i = 0, indexOtazky = 0; i < line; i++) // prochazeni dat
+    {
+        for (int j = 0; j < MAX_LENGTH; j++)
+        {
+            if (data[i][j] == '[')
+            {
+                for (int k = 1; data[i][j + k] != ']'; k++)
+                {
+                    otazky[indexOtazky][k - 1] = data[i][j + k];
+                }
+                indexOtazky++;
+            }
+        }
+    }
+    for (int i = 0; (i < sizeof(otazky) / sizeof(otazky[0])) && (otazky[i][0] != '\0'); i++) // vypsani radku
+    {
+        printf("%s\n", otazky[i]); // vypsani radku
+    }
+    printf("random text");
+
+    return 0;
+}
